7a3ca8bab05b5d79fe8f4c86a488a9a9c14e9aca
Prem Kumar M <premkumarm23122005@gmail.com>
2025-04-10 12:38:10 +0530

    Create Define Class Test 10.4.2025 (Python).py

---
+++ Daily Challenge/Default Daily Challenges/Define Class Test 10.4.2025 (Python).py
@@ -0,0 +1,10 @@
+class Test:
+    def __init__(self, name, duration):
+        self.name = name
+        self.duration = duration
+
+    def getName(self):
+        return self.name
+
+    def getDuration(self):
+        return self.duration
