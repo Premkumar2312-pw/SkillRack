906e6be04883366fc52c459a9194af3c7bd8ec48
Prem Kumar M <premkumarm23122005@gmail.com>
2025-05-10 17:51:58 +0530

    Create Define Class BasicCalculator 9.5.2025 (Java).java

---
+++ Daily Challenge/Default Daily Challenges/Define Class BasicCalculator 9.5.2025 (Java).java
@@ -0,0 +1,27 @@
+package com.skillrack;
+
+public class BasicCalculator {
+    public static int addition(int x, int y) {
+        return x + y;
+    }
+
+    public static int subtraction(int x, int y) {
+        return x - y;
+    }
+
+    public static int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    public static int integerDivision(int x, int y) {
+        return x / y;
+    }
+
+    public static double floatDivision(int x, int y) {
+        return (double) x / y;
+    }
+
+    public static int modulo(int x, int y) {
+        return x % y;
+    }
+}
