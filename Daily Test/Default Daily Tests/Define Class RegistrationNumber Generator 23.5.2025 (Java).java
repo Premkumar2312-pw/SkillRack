30281345c451ee41f63d8c6148a825f51dc6a59e
Prem Kumar M <premkumarm23122005@gmail.com>
2025-05-23 08:55:26 +0530

    Create Define Class RegistrationNumber Generator 23.5.2025 (Java).java

---
+++ Daily Test/Default Daily Tests/Define Class RegistrationNumber Generator 23.5.2025 (Java).java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class RegistrationNumberGenerator {
+    private Map<String, Integer> regCount;
+
+    public RegistrationNumberGenerator() {
+        regCount = new HashMap<>();
+    }
+
+    public String generatRegistrationNumber(int year, char section) {
+        String key = year + "" + section;
+        int count = regCount.getOrDefault(key, 0) + 1;
+        regCount.put(key, count);
+
+        return String.format("%d%c%03d", year, section, count);
+    }
+}
