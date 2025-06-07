62c43c7f4616604a8d55035db80a772bd42f218a
Prem Kumar M <premkumarm23122005@gmail.com>
2025-06-07 09:28:24 +0530

    Create Fibonacci series Parentheses 7.6.2025.cpp

---
+++ Daily Test/C++/Fibonacci series Parentheses 7.6.2025.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+long long len[31];
+
+char f(int n, long long x) {
+    if (n <= 2)
+        return x == 1 ? '(' : ')';
+    if (x == 1) return '(';
+    if (x == len[n]) return ')';
+    if (x <= 1 + len[n - 1]) return f(n - 1, x - 1);
+    return f(n - 2, x - 1 - len[n - 1]);
+}
+
+int main() {
+    int n;
+    long long x;
+    cin >> n >> x;
+
+    len[1] = len[2] = 2;
+    for (int i = 3; i <= n; i++)
+        len[i] = 2 + len[i - 1] + len[i - 2];
+
+    cout << f(n, x) << endl;
+    return 0;
+}
