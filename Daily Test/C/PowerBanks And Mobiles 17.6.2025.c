2731c218e54b82e95286e933365cec6154eb9219
Prem Kumar M <premkumarm23122005@gmail.com>
2025-06-17 09:16:34 +0530

    Update PowerBanks And Mobiles 17.6.2025.c

--- Daily Test/C/PowerBanks And Mobiles 17.6.2025.c
+++ Daily Test/C/PowerBanks And Mobiles 17.6.2025.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int cmp(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
+int compare(const void *a, const void *b) {
+    return *(int*)a - *(int*)b;
 }
 
 int main() {
-    int n, m, i, j, c = 0;
+    int n, c = 0;
     scanf("%d", &n);
-    int p[n];
-    for (i = 0; i < n; i++) scanf("%d", &p[i]);
-
-    scanf("%d", &m);
-    int q[m];
-    for (i = 0; i < m; i++) scanf("%d", &q[i]);
-
-    qsort(p, n, sizeof(int), cmp);
-    qsort(q, m, sizeof(int), cmp);
-
-    for (i = 0; i < m; i++) {
-        int need = 100 - q[i];
-        for (j = 0; j < n; j++) {
-            if (p[j] >= need) {
-                p[j] -= need;
-                c++;
-                break;
-            }
-        }
+    for (int i = 0, x; i < n; i++) {
+        scanf("%d", &x);
+        c += x;
     }
-    printf("%d\n", c);
+
+    int m;
+    scanf("%d", &m);
+    int a[m];
+    for (int i = 0, x; i < m; i++) {
+        scanf("%d", &x);
+        a[i] = 100 - x;
+    }
+
+    qsort(a, m, sizeof(int), compare);
+
+    int count = 0;
+    for (int i = 0; i < m; i++) {
+        if (c - a[i] >= 0) {
+            c -= a[i];
+            count++;
+        } else break;
+    }
+
+    printf("%d\n", count);
     return 0;
 }
