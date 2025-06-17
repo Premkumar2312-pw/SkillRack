272174809e5d91b651560c111faa2f2d2e7719dd
Prem Kumar M <premkumarm23122005@gmail.com>
2025-06-17 09:16:01 +0530

    Update PowerBanks And Mobiles 17.6.2025.py

--- Daily Test/Python/PowerBanks And Mobiles 17.6.2025.py
+++ Daily Test/Python/PowerBanks And Mobiles 17.6.2025.py
@@ -1,15 +1,3 @@
-n = int(input())
-p = sorted(map(int, input().split()))
-m = int(input())
-q = sorted(map(int, input().split()))
-c = 0
-
-for x in q:
-    r = 100 - x
-    for i in range(n):
-        if p[i] >= r:
-            p[i] -= r
-            c += 1
-            break
-
-print(c)
+input(); c = sum(map(int, input().split()))
+input(); m = sorted(100 - int(x) for x in input().split())
+print(sum((c := c - i) >= 0 for i in m))
