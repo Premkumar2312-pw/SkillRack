09e92ea23334f5a594d9452a69064ed7738977f7
Prem Kumar M <premkumarm23122005@gmail.com>
2025-06-17 17:55:37 +0530

    Update Doctor Visting Slots-Two Cities 17.6.2025.cpp

--- Daily Challenge/C++/Doctor Visting Slots-Two Cities 17.6.2025.cpp
+++ Daily Challenge/C++/Doctor Visting Slots-Two Cities 17.6.2025.cpp
@@ -1,51 +1,58 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
 #include <ctime>
 using namespace std;
 
-string get_date_string(tm dt) {
+string format_date(struct tm date) {
     const char* months[] = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-    char buf[20];
-    sprintf(buf, "%02d-%s-%d", dt.tm_mday, months[dt.tm_mon], dt.tm_year + 1900);
-    return string(buf);
-}
-
-tm add_days(tm dt, int days) {
-    time_t t = mktime(&dt);
-    t += days * 86400;
-    return *localtime(&t);
+    char buffer[20];
+    sprintf(buffer, "%02d-%s-%04d", date.tm_mday, months[date.tm_mon], date.tm_year + 1900);
+    return string(buffer);
 }
 
 int main() {
-    int M, N, Y;
-    cin >> M >> N >> Y;
+    int m, n, y;
+    cin >> m >> n >> y;
 
-    tm curr = {}, end = {};
-    curr.tm_year = end.tm_year = Y - 1900;
-    curr.tm_mon = 0; curr.tm_mday = 1;
-    end.tm_mon = 11; end.tm_mday = 31;
-    mktime(&curr); mktime(&end);
+    tm dt = {};
+    dt.tm_year = y - 1900;
+    dt.tm_mon = 0;
+    dt.tm_mday = 1;
 
-    vector<string> A, B;
-    bool f = true;
+    tm ed = {};
+    ed.tm_year = y - 1900;
+    ed.tm_mon = 11;
+    ed.tm_mday = 31;
 
-    while (mktime(&curr) <= mktime(&end)) {
-        int t = f ? M : N;
-        tm nd = add_days(curr, t - 1);
-        if (mktime(&nd) > mktime(&end)) nd = end;
+    time_t end_time = mktime(&ed);
+    char city = 'A';
 
-        string s = get_date_string(curr) + " to " + get_date_string(nd);
-        if (f) A.push_back(s);
-        else B.push_back(s);
+    vector<pair<string, string>> cityA, cityB;
 
-        curr = add_days(nd, 1);
-        f = !f;
+    while (mktime(&dt) <= end_time) {
+        tm start = dt;
+        int days = (city == 'A') ? m : n;
+
+        dt.tm_mday += days - 1;
+        mktime(&dt);
+        if (mktime(&dt) > end_time)
+            dt = ed;
+
+        if (city == 'A') cityA.push_back({format_date(start), format_date(dt)});
+        else cityB.push_back({format_date(start), format_date(dt)});
+
+        dt.tm_mday += 1;
+        mktime(&dt);
+        city = (city == 'A') ? 'B' : 'A';
     }
 
     cout << "City A:\n";
-    for (auto& s : A) cout << s << "\n";
+    for (auto [s, t] : cityA)
+        cout << s << " to " << t << endl;
+
     cout << "City B:\n";
-    for (auto& s : B) cout << s << "\n";
+    for (auto [s, t] : cityB)
+        cout << s << " to " << t << endl;
+
     return 0;
 }
