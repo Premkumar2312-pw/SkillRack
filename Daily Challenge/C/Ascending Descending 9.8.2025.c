2b447cbc2c71d5ab262bd2b363d827109b08f0cf
Prem Kumar M <premkumarm23122005@gmail.com>
2025-07-09 17:34:59 +0530

    Update Ascending Descending 9.8.2025.c

--- Daily Challenge/C/Ascending Descending 9.8.2025.c
+++ Daily Challenge/C/Ascending Descending 9.8.2025.c
@@ -1,45 +1,50 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int is_sorted_asc(int* arr, int n) {
-    for (int i = 1; i < n; i++)
-        if (arr[i - 1] > arr[i]) return 0;
+int is_asc(int arr[], int n) {
+    for(int i = 0; i < n-1; i++)
+        if(arr[i] > arr[i+1]) return 0;
     return 1;
 }
 
-int all_equal(int* a, int m, int* b, int n) {
-    int val = a[0];
-    for (int i = 0; i < m; i++) if (a[i] != val) return 0;
-    for (int i = 0; i < n; i++) if (b[i] != val) return 0;
+int is_desc(int arr[], int n) {
+    for(int i = 0; i < n-1; i++)
+        if(arr[i] < arr[i+1]) return 0;
     return 1;
 }
 
-void print(int* arr, int n) {
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+int all_equal(int arr[], int n) {
+    for(int i = 1; i < n; i++)
+        if(arr[i] != arr[0]) return 0;
+    return 1;
+}
+
+void print_array(int arr[], int n) {
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
     printf("\n");
 }
 
 int main() {
     int m, n, a[100], b[100];
     scanf("%d", &m);
-    for (int i = 0; i < m; i++) scanf("%d", &a[i]);
+    for(int i = 0; i < m; i++) scanf("%d", &a[i]);
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) scanf("%d", &b[i]);
+    for(int i = 0; i < n; i++) scanf("%d", &b[i]);
 
-    int sa = is_sorted_asc(a, m), sb = is_sorted_asc(b, n);
-
-    if (all_equal(a, m, b, n)) {
-        print(a, m);
-        print(b, n);
-    } else if (sa && !sb) {
-        print(a, m);
-        print(b, n);
-    } else if (!sa && sb) {
-        print(b, n);
-        print(a, m);
-    } else {
-        print(a, m);
-        print(b, n);
+    int ae = all_equal(a, m), be = all_equal(b, n);
+    if (ae && be) {
+        if (a[0] < b[0]) { print_array(a, m); print_array(b, n); }
+        else if (b[0] < a[0]) { print_array(b, n); print_array(a, m); }
+        else { print_array(a, m); print_array(b, n); }
+    }
+    else if (is_asc(a, m) && is_desc(b, n)) {
+        print_array(a, m); print_array(b, n);
+    }
+    else if (is_desc(a, m) && is_asc(b, n)) {
+        print_array(b, n); print_array(a, m);
+    }
+    else {
+        print_array(a, m); print_array(b, n);
     }
     return 0;
 }
