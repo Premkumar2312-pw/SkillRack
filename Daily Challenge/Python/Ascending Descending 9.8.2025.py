f494700d793770e062406a5e0a14bc46dd41c8fe
Prem Kumar M <premkumarm23122005@gmail.com>
2025-07-09 17:34:34 +0530

    Update Ascending Descending 9.8.2025.py

--- Daily Challenge/Python/Ascending Descending 9.8.2025.py
+++ Daily Challenge/Python/Ascending Descending 9.8.2025.py
@@ -3,16 +3,23 @@ a = list(map(int, input().split()))
 n = int(input())
 b = list(map(int, input().split()))
 
-sa = a == sorted(a)
-sb = b == sorted(b)
+def is_asc(x): return all(x[i] <= x[i+1] for i in range(len(x)-1))
+def is_desc(x): return all(x[i] >= x[i+1] for i in range(len(x)-1))
 
-if all(x == a[0] for x in a + b):
+if all(x == a[0] for x in a) and all(x == b[0] for x in b):
+    if a[0] < b[0]:
+        print(*a)
+        print(*b)
+    elif b[0] < a[0]:
+        print(*b)
+        print(*a)
+    else:
+        print(*a)
+        print(*b)
+elif is_asc(a) and is_desc(b):
     print(*a)
     print(*b)
-elif sa and not sb:
-    print(*a)
-    print(*b)
-elif not sa and sb:
+elif is_desc(a) and is_asc(b):
     print(*b)
     print(*a)
 else:
