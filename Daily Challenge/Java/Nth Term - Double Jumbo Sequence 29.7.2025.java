d7fd0832ea123cd120311a555a753a23a686c969
Prem Kumar M <premkumarm23122005@gmail.com>
2025-07-29 19:00:08 +0530

    Create Nth Term - Double Jumbo Sequence 29.7.2025.java

---
+++ Daily Challenge/Java/Nth Term - Double Jumbo Sequence 29.7.2025.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] s = new int[n];
+        s[0] = 0; s[1] = 1;
+        for (int i = 2; i < n; i++)
+            s[i] = s[i - 1] + 2 * s[i - 2] + 3;
+        System.out.println(s[n - 1]);
+    }
+}
