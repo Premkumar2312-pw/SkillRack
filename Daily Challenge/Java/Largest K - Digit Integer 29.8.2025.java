a0381cd6ade560e41e269a1b1d8358d44706d5ef
Prem Kumar M <premkumarm23122005@gmail.com>
2025-08-29 21:35:39 +0530

    Update Largest K - Digit Integer 29.8.2025.java

--- Daily Challenge/Java/Largest K - Digit Integer 29.8.2025.java
+++ Daily Challenge/Java/Largest K - Digit Integer 29.8.2025.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String s=sc.next();
-        int[] w=new int[26];
-        w[0]=0; w[1]=1;
-        for(int i=2;i<26;i++) w[i]=w[i-2]+w[i-1];
-        int sum=0;
-        for(char c : s.toLowerCase().toCharArray()) {
-            sum+=w[c-'a'];
+        String n=sc.next();
+        int k=sc.nextInt();
+        if(n.length() < k) {
+            System.out.println(-1);
+            return;
         }
-        System.out.println(sum);
+        int max=-1;
+        for(int i=0; i<=n.length()-k; i++) {
+            int val = Integer.parseInt(n.substring(i,i+k));
+            if(val > max) max=val;
+        }
+        System.out.println(max);
     }
 }
