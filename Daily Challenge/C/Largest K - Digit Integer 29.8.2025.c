1f456c59d15ef584c3af6ec9b5f2fb15bba49239
Prem Kumar M <premkumarm23122005@gmail.com>
2025-08-29 21:34:31 +0530

    Update Largest K - Digit Integer 29.8.2025.c

--- Daily Challenge/C/Largest K - Digit Integer 29.8.2025.c
+++ Daily Challenge/C/Largest K - Digit Integer 29.8.2025.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 int main() {
-    char s[101];
-    scanf("%s", s);
-    int w[26];
-    w[0]=0; w[1]=1;
-    for(int i=2; i<26; i++) w[i] = w[i-2]+w[i-1];
-    int sum=0, n = strlen(s);
-    for(int i=0; i<n; i++) {
-        char c = tolower(s[i]);
-        sum += w[c-'a'];
+    char n[20];
+    int k;
+    scanf("%s", n);
+    scanf("%d", &k);
+    int len = strlen(n);
+    if (len < k) {
+        printf("-1\n");
+        return 0;
     }
-    printf("%d\n", sum);
+    int max = -1;
+    for (int i = 0; i <= len - k; i++) {
+        char sub[20];
+        strncpy(sub, n + i, k);
+        sub[k] = '\0';
+        int val = atoi(sub);
+        if (val > max) max = val;
+    }
+    printf("%d\n", max);
     return 0;
 }
