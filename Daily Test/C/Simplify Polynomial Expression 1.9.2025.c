cdf572444042c1e2555d7047c1cbad10686e6e4c
Prem Kumar M <premkumarm23122005@gmail.com>
2025-09-01 20:25:52 +0530

    Create Simplify Polynomial Expression 1.9.2025.c

---
+++ Daily Test/C/Simplify Polynomial Expression 1.9.2025.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef struct {
+    int coef;
+    int exp;
+} Term;
+
+int cmp(const void *a, const void *b) {
+    return ((Term*)b)->exp - ((Term*)a)->exp;
+}
+
+int main() {
+    char S[1000];
+    scanf("%s", S);
+
+    Term terms[100];
+    int n = 0;
+
+    for (char *p = strtok(S, "+-"); p; p = strtok(NULL, "+-")) {
+        int coef, exp;
+        sscanf(p, "%dx^%d", &coef, &exp);
+        terms[n].coef = coef;
+        terms[n].exp = exp;
+        n++;
+    }
+
+    qsort(terms, n, sizeof(Term), cmp);
+
+    int flag = 0;
+    for (int i = 0; i < n; i++) {
+        if (terms[i].coef == 0) continue;
+        if (terms[i].coef > 0 && flag) printf("+");
+        printf("%d%s^%d", terms[i].coef, "x", terms[i].exp);
+        flag = 1;
+    }
+
+    if (!flag) printf("0");
+    return 0;
+}
