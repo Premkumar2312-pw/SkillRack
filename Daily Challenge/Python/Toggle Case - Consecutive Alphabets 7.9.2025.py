fc29742af73c8c65056ebb1f34fece464c457121
Prem Kumar M <premkumarm23122005@gmail.com>
2025-09-07 22:59:00 +0530

    Create Toggle Case - Consecutive Alphabets 7.9.2025.py

---
+++ Daily Challenge/Python/Toggle Case - Consecutive Alphabets 7.9.2025.py
@@ -0,0 +1,15 @@
+s = input().strip()
+out = []
+i, n = 0, len(s)
+
+while i < n:
+    j = i + 1
+    while j < n and (s[i].isupper() == s[j].isupper()):
+        j += 1
+    if j - i >= 2:
+        out.append(s[i:j].swapcase())
+    else:
+        out.append(s[i])
+    i = j
+
+print("".join(out))
