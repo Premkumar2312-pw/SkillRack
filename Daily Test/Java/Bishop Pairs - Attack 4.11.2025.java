396236706adad259b239d04d430c3078423785e0
Prem Kumar M <premkumarm23122005@gmail.com>
2025-11-04 09:53:39 +0530

    Update Bishop Pairs - Attack 4.11.2025.java

--- Daily Test/Java/Bishop Pairs - Attack 4.11.2025.java
+++ Daily Test/Java/Bishop Pairs - Attack 4.11.2025.java
@@ -5,21 +5,28 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String[][] board = new String[n][n];
-
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
                 board[i][j] = sc.next();
-
         int res = 0;
-        // main diagonal
-        for (int i = 0; i < n; i++)
-            if (board[i][i].equals(board[0][0]))
-                res++;
-        // anti diagonal
-        for (int i = 0; i < n; i++)
-            if (board[i][n - 1 - i].equals(board[0][n - 1]))
-                res++;
-
+        int[] diag = {1, 1};
+        int[] rev = {1, -1};
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(board[i][j].equals("1")) {
+                    int nc = i + diag[0], nr = j + diag[1];
+                    while(nc >= 0 && nc < n && nr >= 0 && nr < n) {
+                        if(board[nc][nr].equals("1")) res++;
+                        nc++; nr++;
+                    }
+                    int ec = i + rev[0], er = j + rev[1];
+                    while(ec >= 0 && ec < n && er >= 0 && er < n) {
+                        if(board[ec][er].equals("1")) res++;
+                        ec++; er--;
+                    }
+                }
+            }
+        }
         System.out.println(res);
         sc.close();
     }
