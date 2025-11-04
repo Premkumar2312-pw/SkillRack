556c0757d7c3abd00f9dd3bcb8433d5ee3894bb4
Prem Kumar M <premkumarm23122005@gmail.com>
2025-11-04 09:53:21 +0530

    Update Bishop Pairs - Attack 4.11.2025.cpp

--- Daily Test/C++/Bishop Pairs - Attack 4.11.2025.cpp
+++ Daily Test/C++/Bishop Pairs - Attack 4.11.2025.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
 int main() {
@@ -10,17 +9,29 @@ int main() {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             cin >> board[i][j];
-
     int res = 0;
-    // main diagonal
-    for (int i = 0; i < n; i++)
-        if (board[i][i] == board[0][0])
-            res++;
-    // anti diagonal
-    for (int i = 0; i < n; i++)
-        if (board[i][n - 1 - i] == board[0][n - 1])
-            res++;
-
+    int diag[2] = {1, 1};
+    int rev[2] = {1, -1};
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (board[i][j] == "1") {
+                int nc = i + diag[0], nr = j + diag[1];
+                while (nc >= 0 && nc < n && nr >= 0 && nr < n) {
+                    if (board[nc][nr] == "1")
+                        res++;
+                    nc++;
+                    nr++;
+                }
+                int ec = i + rev[0], er = j + rev[1];
+                while (ec >= 0 && ec < n && er >= 0 && er < n) {
+                    if (board[ec][er] == "1")
+                        res++;
+                    ec++;
+                    er--;
+                }
+            }
+        }
+    }
     cout << res << endl;
     return 0;
 }
