b231469350b8facf8c726c562efa60bfa9f9076f
Prem Kumar M <premkumarm23122005@gmail.com>
2025-11-04 09:52:57 +0530

    Update Bishop Pairs - Attack 4.11.2025.c

--- Daily Test/C/Bishop Pairs - Attack 4.11.2025.c
+++ Daily Test/C/Bishop Pairs - Attack 4.11.2025.c
@@ -4,23 +4,33 @@
 int main() {
     int n;
     scanf("%d", &n);
-    char board[n][n][11]; // assuming each cell can go up to 10 chars
-
+    char board[n][n][5];
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             scanf("%s", board[i][j]);
-
     int res = 0;
-    // main diagonal (top-left to bottom-right)
-    for (int i = 0; i < n; i++)
-        if (strcmp(board[i][i], board[0][0]) == 0)
-            res++;
-
-    // anti diagonal (top-right to bottom-left)
-    for (int i = 0; i < n; i++)
-        if (strcmp(board[i][n-1-i], board[0][n-1]) == 0)
-            res++;
-
+    int diag[2] = {1, 1};
+    int rev[2] = {1, -1};
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (strcmp(board[i][j], "1") == 0) {
+                int nc = i + diag[0], nr = j + diag[1];
+                while (nc >= 0 && nc < n && nr >= 0 && nr < n) {
+                    if (strcmp(board[nc][nr], "1") == 0)
+                        res++;
+                    nc++;
+                    nr++;
+                }
+                int ec = i + rev[0], er = j + rev[1];
+                while (ec >= 0 && ec < n && er >= 0 && er < n) {
+                    if (strcmp(board[ec][er], "1") == 0)
+                        res++;
+                    ec++;
+                    er--;
+                }
+            }
+        }
+    }
     printf("%d
 ", res);
     return 0;
