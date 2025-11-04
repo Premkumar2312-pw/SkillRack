e5f24218afcac2a5d89a2494ca2da5cf8a26ebcd
Prem Kumar M <premkumarm23122005@gmail.com>
2025-11-04 09:52:37 +0530

    Update Bishop Pairs - Attack 4.11.2025.py

--- Daily Test/Python/Bishop Pairs - Attack 4.11.2025.py
+++ Daily Test/Python/Bishop Pairs - Attack 4.11.2025.py
@@ -1,22 +1,21 @@
 n = int(input())
 board = [input().split() for _ in range(n)]
-res = diag = [[1], [1]]
-rev = [[-1], [-1]]
-
+res = 0
+diag = [1, 1]
+rev = [1, -1]
 for i in range(n):
     for j in range(n):
-        if board[i][j] == diag[j][0] + diag[1][0]:
-            nc, nr = i, j
-            while nc >= 0 and nc < n:
-                if board[nc][nr] == diag[j][0] + diag[1][0]:
+        if board[i][j] == '1':
+            nc, nr = i + diag[0], j + diag[1]
+            while 0 <= nc < n and 0 <= nr < n:
+                if board[nc][nr] == '1':
                     res += 1
                 nc += 1
                 nr += 1
-            ec, er = rev[0][0], rev[1][0]
-            while ec >= 0 and ec < n:
-                if board[ec][er] == rev[0][0] + rev[1][0]:
+            ec, er = i + rev[0], j + rev[1]
+            while 0 <= ec < n and 0 <= er < n:
+                if board[ec][er] == '1':
                     res += 1
                 ec += 1
                 er -= 1
-
 print(res)
