c2a6d2da680e784b3e2eaf3a38ea8bb6d0896d80
Prem Kumar M <premkumarm23122005@gmail.com>
2026-02-17 19:40:39 +0530

    Refactor city missile handling with arrays
    Refactored city missile logic to use arrays instead of structs.

--- Daily Test/C/City Missiles 17.2.2026.c
+++ Daily Test/C/City Missiles 17.2.2026.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef struct {
-    int x, y;
-} City;
+#define MAX 100000
+
+int keys[MAX];
+int values[MAX];
+int size = 0;
+
+int find(int key) {
+    for (int i = 0; i < size; i++) {
+        if (keys[i] == key)
+            return i;
+    }
+    return -1;
+}
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    City *cities = malloc(n * sizeof(City));
-
     for (int i = 0; i < n; i++) {
-        scanf("%d %d", &cities[i].x, &cities[i].y);
+        int x, y;
+        scanf("%d %d", &x, &y);
+        keys[size] = x;
+        values[size] = x + y;
+        size++;
     }
 
-    for (int i = 0; i < n; i++) {
-        int x1 = cities[i].x;
-        int y1 = cities[i].y;
-        int target = x1 + y1;
+    int f = 0;
 
-        for (int j = 0; j < n; j++) {
-            if (cities[j].x == target) {
-                int y2 = cities[j].y;
-                if (target - y2 == x1) {
-                    printf("YES\n");
-                    free(cities);
-                    return 0;
-                }
-            }
+    for (int i = 0; i < size; i++) {
+        int x = keys[i];
+        int tar = values[i];
+
+        int idx = find(tar);
+        if (idx != -1 && values[idx] == x) {
+            f = 1;
+            break;
         }
     }
 
-    printf("NO\n");
-    free(cities);
+    if (f)
+        printf("YES\n");
+    else
+        printf("NO\n");
+
     return 0;
 }
