98676de867bffa7388c6ece897d74ef428f71122
Prem Kumar M <premkumarm23122005@gmail.com>
2026-02-17 19:39:04 +0530

    Refactor city missile logic and input handling

--- Daily Test/Python/City Missiles 17.2.2026.py
+++ Daily Test/Python/City Missiles 17.2.2026.py
@@ -1,21 +1,16 @@
-n = int(input().strip())
-cities = []
+n = int(input())
+att = {}
+
 for _ in range(n):
     x, y = map(int, input().split())
-    cities.append((x, y))
+    att[x] = x + y
 
-positions = {x: y for x, y in cities}
-found = False
+f = False
 
-for x1, y1 in cities:
-    target = x1 + y1
-    if target in positions:
-        y2 = positions[target]
-        if target - y2 == x1:
-            found = True
-            break
+for x in att:
+    tar = att[x]
+    if tar in att and att[tar] == x:
+        f = True
+        break
 
-if found:
-    print("YES")
-else:
-    print("NO")
+print("YES" if f else "NO")
