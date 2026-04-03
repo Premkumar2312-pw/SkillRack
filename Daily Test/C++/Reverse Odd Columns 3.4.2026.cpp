b8d7fc5c2a96b155c7ea00c77d35dc19d7fcd979
Prem Kumar M <premkumarm23122005@gmail.com>
2026-04-03 17:01:35 +0530

    Create Reverse Odd Columns 3.4.2026.cpp

---
+++ Daily Test/C++/Reverse Odd Columns 3.4.2026.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> mat(n, vector<int>(n));
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> mat[i][j];
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(j % 2 == 0)
+                cout << mat[n-1-i][j] << " ";
+            else
+                cout << mat[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
