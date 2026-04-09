490631759919d26939b8f098b2e0fe568fc86a48
Prem Kumar M <premkumarm23122005@gmail.com>
2026-04-09 10:18:13 +0530

    Create Pattern Printing 9.4.2026.c

---
+++ Daily Challenge/C/Pattern Printing 9.4.2026.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++)
+            printf("%d",i);
+        printf("\n");
+    }
+
+    for(int i=n;i>=1;i--){
+        for(int j=0;j<i;j++)
+            printf("%d",i);
+        printf("\n");
+    }
+
+    return 0;
+}
