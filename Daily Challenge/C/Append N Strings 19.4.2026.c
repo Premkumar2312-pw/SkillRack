61ee521e71dc19c3349d5ed94a9018dd020487d4
Prem Kumar M <premkumarm23122005@gmail.com>
2026-04-19 07:54:19 +0530

    Create Append N Strings 19.4.2026.c

---
+++ Daily Challenge/C/Append N Strings 19.4.2026.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    char str[n][100];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%s", str[i]);
+    }
+
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%s", str[i]);
+    }
+
+    return 0;
+}
